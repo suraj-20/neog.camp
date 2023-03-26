57addd80dfc87f550eddf7999d2fce624136761c
Suraj <kumarsuraj54057@gmail.com>
2023-03-26 13:56:50 +0530

    ex04: wiring textarea input

--- LessonFour/script.js
+++ LessonFour/script.js
@@ -1,5 +1,7 @@
 var btnClick = document.querySelector("#btn-click");
+var txtInput = document.getElementById("txt-input");
 
 btnClick.addEventListener("click", function clickHandler() {
     console.log("Clicked!");
+    console.log(txtInput.value);
 })
