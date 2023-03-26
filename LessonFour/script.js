4c9a336d96a92902b983ced1eed7ca8361ef886b
Suraj <kumarsuraj54057@gmail.com>
2023-03-26 14:37:42 +0530

    live exercise: querySelector

--- LessonFour/script.js
+++ LessonFour/script.js
@@ -5,3 +5,22 @@ btnClick.addEventListener("click", function clickHandler() {
     console.log("Clicked!");
     console.log(txtInput.value);
 })
+
+
+// var textAreaInput = document.querySelector("textarea");
+// var primaryBtn = document.querySelector(".btn-primary");
+
+// primaryBtn.addEventListener('click', function onClick() {
+//     console.log("Primary btn clicked");
+//     console.log(textAreaInput.value);
+    
+// })
+// var inputBtn = document.querySelector("#input-btn");
+
+// inputBtn.addEventListener('click', function inputbtn() {
+//     console.log("input btn clicked");
+// })
+
+
+// var translator = document.querySelector("[translator]");
+
