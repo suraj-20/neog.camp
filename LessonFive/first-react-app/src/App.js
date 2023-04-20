6672978fef4c47983d06fb793a906a8d0c5e5ab0
Suraj <kumarsuraj54057@gmail.com>
2023-04-20 14:41:02 +0530

    used state and setState properties

--- LessonFive/first-react-app/src/App.js
+++ LessonFive/first-react-app/src/App.js
@@ -2,18 +2,21 @@ import { Component } from "react";
 import "./App.css";
 import Greet from "./Components/Greet";
 import Welcome from './Components/Welcome';
-
+import Message from "./Components/Message";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello world</h1>
-        <Greet name="Java" />
+
+        <Message />
+
+        {/* <Greet name="Java" />
         <Greet name="Bruce" />
         <Greet name="Diana" />
         <Welcome name="JAva" />
         <Welcome name="Bruce" />
-        <Welcome name="Diana" />
+        <Welcome name="Diana" /> */}
       </div>
     );
   }
