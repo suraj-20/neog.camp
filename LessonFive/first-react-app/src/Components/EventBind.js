4591a8e61630b8d082cb54641a02aa32ceb6abc6
Suraj <kumarsuraj54057@gmail.com>
2023-04-24 13:55:30 +0530

    second approach of eventBinding

--- LessonFive/first-react-app/src/Components/EventBind.js
+++ LessonFive/first-react-app/src/Components/EventBind.js
@@ -21,7 +21,8 @@ class EventBind extends Component {
     return (
       <div>
         <div>{this.state.message}</div>
-        <button onClick={this.clickHandler.bind(this)}>Click</button>
+        {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+        <button onClick={() => this.clickHandler()}>Click</button>
       </div>
     )
   }
