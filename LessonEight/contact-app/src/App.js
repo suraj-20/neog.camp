c4e700ab08f976358617b4968812bdfe121d50ce
Suraj <kumarsuraj54057@gmail.com>
2023-04-29 13:17:37 +0530

    Contact manager app

--- LessonEight/contact-app/src/App.js
+++ LessonEight/contact-app/src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {v4 as uuid} from "uuid"; 
+import './App.css'
+import Header from "./Components/Header";
+import Addcontact from "./Components/Addcontact";
+import ContactList from "./Components/ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, {id: uuid(), ...contact}]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) =>{
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
+  }
+
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+  
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retriveContacts) setContacts(retriveContacts);
+    console.log(retriveContacts);
+  }, [])
+
   return (
-    <div className="App">
-      Hello world
+    <div className="ui container">
+      <Header />
+      <Addcontact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId = {removeContactHandler} />
     </div>
   );
 }
